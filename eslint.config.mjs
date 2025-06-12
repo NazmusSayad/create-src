4aeb5e64618e85d9ac95d76e1d61ff0cd474818e
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-06-13 04:10:54 +0600

    chore: update ESLint configuration for improved compatibility

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,7 @@ const compat = new FlatCompat({
 
 export default [
   {
-    ignores: ['./*.{js,mjs,ts}', '**/node_modules', '**/dist'],
+    ignores: ['./*.{js,mjs,ts}', '**/node_modules', '**/dist', '**/templates'],
   },
 
   ...configs.recommended,
