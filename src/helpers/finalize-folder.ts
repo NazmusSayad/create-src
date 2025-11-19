335b69ddeff75c049d8193f4ae95141435bba232
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-11-19 19:24:03 +0600

    Update version to 1.0.8 in package.json and enhance finalizeFolder function to allow selection of package manager.

--- src/helpers/finalize-folder.ts
+++ src/helpers/finalize-folder.ts
@@ -1,18 +1,42 @@
-import { confirm } from '@inquirer/prompts'
+import { select } from '@inquirer/prompts'
 import kleur from 'kleur'
 import { execShellCommand } from '../utils/shell'
 
 export async function finalizeFolder(cwd: string) {
   console.log('')
 
-  const shouldFinalize = await confirm({
-    message: 'Finalize your project?',
-    default: true,
+  const packageManager = await select({
+    message: 'Pick your favorite package manager?',
+    choices: [
+      {
+        name: 'pnpm',
+        value: 'pnpm',
+        description:
+          'pnpm is a fast, disk space efficient package manager for JavaScript.',
+      },
+      {
+        name: 'npm',
+        value: 'npm',
+        description: 'npm is the default package manager for Node.js.',
+      },
+      {
+        name: 'yarn',
+        value: 'yarn',
+        description:
+          'yarn is a fast, reliable, and secure package manager for JavaScript.',
+      },
+      {
+        name: 'bun',
+        value: 'bun',
+        description:
+          'bun is a fast, modern, and secure package manager for JavaScript.',
+      },
+    ],
   })
 
-  if (shouldFinalize) {
+  if (packageManager) {
     await execShellCommand(cwd, 'git', 'init')
-    await execShellCommand(cwd, 'npm', 'install')
+    await execShellCommand(cwd, packageManager, 'install')
     await execShellCommand(cwd, 'npx', 'eslint', '--fix', '.')
   }
 
