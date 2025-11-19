10bce68395f5206a52c1536ae180760ebf0202a4
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-11-20 00:03:21 +0600

    Refactor finalizeFolder function to remove package manager selection and default to npm for installation, streamlining the setup process.

--- src/helpers/finalize-folder.ts
+++ src/helpers/finalize-folder.ts
@@ -1,44 +1,12 @@
-import { select } from '@inquirer/prompts'
 import kleur from 'kleur'
 import { execShellCommand } from '../utils/shell'
 
 export async function finalizeFolder(cwd: string) {
   console.log('')
 
-  const packageManager = await select({
-    message: 'Pick your favorite package manager?',
-    choices: [
-      {
-        name: 'Pnpm',
-        value: 'pnpm',
-        description:
-          'pnpm is a fast, disk space efficient package manager for JavaScript.',
-      },
-      {
-        name: 'Npm',
-        value: 'npm',
-        description: 'npm is the default package manager for Node.js.',
-      },
-      {
-        name: 'Yarn',
-        value: 'yarn',
-        description:
-          'yarn is a fast, reliable, and secure package manager for JavaScript.',
-      },
-      {
-        name: 'Bun',
-        value: 'bun',
-        description:
-          'bun is a fast, modern, and secure package manager for JavaScript.',
-      },
-    ],
-  })
-
-  if (packageManager) {
-    await execShellCommand(cwd, 'git', 'init')
-    await execShellCommand(cwd, packageManager, 'install')
-    await execShellCommand(cwd, 'npx', 'eslint', '--fix', '.')
-  }
+  await execShellCommand(cwd, 'git', 'init')
+  await execShellCommand(cwd, 'npm', 'install')
+  await execShellCommand(cwd, 'npx', 'eslint', '--fix', '.')
 
   console.log(kleur.green('Done!'))
 }
